f45951b82bbea3f91b3662fdf4d8c9a5ff0e72da
Ashleen <ashleen.daly@glasgow.ac.uk>
2024-09-16 10:31:34 +0100

    change colours again

--- src/utils/crime-data.ts
+++ src/utils/crime-data.ts
@@ -16,14 +16,13 @@ export const getInterpolations = (min: number, max: number) => {
   const step = total / 5;
 
   const colours = [
-    "rgba(255, 0, 0, 1)",    // Bright Red
-    "rgba(255, 100, 0, 1)",  // Reddish-Orange
-    "rgba(255, 165, 0, 1)",  // Orange (more neutral)
-    "rgba(255, 200, 100, 1)",// Light Orange
-    "rgba(135, 206, 250, 1)",// Light Sky Blue
-    "rgba(173, 216, 230, 1)" // Light Blue
+    "rgba(255, 0, 0, 1)", // Red
+    "rgba(255, 165, 0, 1)", // Orange
+    "rgba(255, 200, 100, 1)", // Neutral Orange
+    "rgba(135, 206, 235, 1)", // Neutral Blue
+    "rgba(173, 216, 230, 1)", // Light Blue
+    "rgba(224, 255, 255, 1)", // Lighter Blue
   ];
-  
 
   const interpolations = [];
   for (let i = 0; i <= 5; i++) {
@@ -33,6 +32,6 @@ export const getInterpolations = (min: number, max: number) => {
   return interpolations;
 };
 
-export const getCrimeRankForZone = (dataZone: string, data:CrimeData[]) => {
-  return data.find((d) => d.Data_Zone === dataZone)?.SIMD2020_Crime_Domain_Rank
+export const getCrimeRankForZone = (dataZone: string, data: CrimeData[]) => {
+  return data.find((d) => d.Data_Zone === dataZone)?.SIMD2020_Crime_Domain_Rank;
 };
