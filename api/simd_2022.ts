778fcc3b1abe2e76dace80c9ded8f21bf8d90c94
Ash <ashddev@proton.me>
2025-05-29 21:45:14 +0100

    test

--- api/simd_2022.ts
+++ api/simd_2022.ts
@@ -1,14 +1,14 @@
-import path from "path";
-import sql from "better-sqlite3";
+// import path from "path";
+// import sql from "better-sqlite3";
 import type { VercelResponse } from "@vercel/node";
 
-const dbPath = path.join(__dirname, "../public/data/simd_2022.db");
-const db = new sql(dbPath, { readonly: true });
+// const dbPath = path.join(__dirname, "../public/data/simd_2022.db");
+// const db = new sql(dbPath, { readonly: true });
 
 export default function handler(response: VercelResponse) {
   try {
-    const crimes = db.prepare("SELECT * FROM simd_2022").all();
-    response.status(200).json(crimes);
+    // const crimes = db.prepare("SELECT * FROM simd_2022").all();
+    response.status(200).json({ test: "hello" });
   } catch (err) {
     response.status(500).json({ error: err.message });
   }
